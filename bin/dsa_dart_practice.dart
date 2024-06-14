3c403e4c8b1516c2ac1149a4cb2dae87efc514c1
chandan pradhan <chandanpradhan@smcindiaonline.com>
2024-06-14 16:02:40 +0530

    Done

---
+++ bin/dsa_dart_practice.dart
@@ -0,0 +1,5 @@
+import 'package:dsa_dart_practice/dsa_dart_practice.dart' as dsa_dart_practice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dsa_dart_practice.calculate()}!');
+}
---
+++ test/dsa_dart_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:dsa_dart_practice/dsa_dart_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
